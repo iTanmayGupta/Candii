c69de6a2fc87fcd2831fa249fccead04e9974322
Anujj <anujjbubna39@gmail.com>
2022-02-06 07:04:38 -0500

    chbasd

--- static/treat/app.js
+++ static/treat/app.js
@@ -1,34 +1,37 @@
-// const ethereumButton = document.querySelector('#enableEthereumButton');
-// const sendEthButton = document.querySelector('#sendMoney');
+"use strict";
 
-// console.log(ethereumButton)
 
-// let accounts = [];
+document.addEventListener('DOMContentLoaded', function() {
 
-// //Sending Ethereum to an address
-// sendEthButton.addEventListener('click', () => {
-//     console.log("Tanmay")
-//   ethereum
-//     .request({
-//       method: 'eth_sendTransaction',
-//       params: [
-//         {
-//           from: accounts[0],
-//           to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
-//           value: '0x29a2241af62c0000',
-//           gasPrice: '0x09184e72a000',
-//           gas: '0x2710',
-//         },
-//       ],
-//     })
-//     .then((txHash) => console.log(txHash))
-//     .catch((error) => console.error);
-// });
+    let amount = document.querySelectorAll(".amountNum")
+    for(let i=0; i<amount.length; i++) {
+        amount[i].addEventListener('click', () => {
+            let amt = amount[i].innerHTML
+            let finalAmount = document.querySelector("#finalAmount")
+            finalAmount.value = amt
+            console.log(finalAmount)
+        })
+    }
 
-// ethereumButton.addEventListener('click', () => {
-//   getAccount();
-// });
-
-// async function getAccount() {
-//   accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-// }
+    let sendMoney = document.querySelector('#sendMoney')
+    sendMoney.addEventListener('click', () => {
+        let finalAmount = document.querySelector("#finalAmount")
+        if(finalAmount.value != null) {
+            ethereum
+                .request({
+                method: 'eth_sendTransaction',
+                params: [
+                    {
+                    from: accounts[0],
+                    to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+                    value: '0x29a2241af62c0000',
+                    gasPrice: '0x09184e72a000',
+                    gas: '0x2710',
+                    },
+                ],
+                })
+                .then((txHash) => console.log(txHash))
+                .catch((error) => console.error);
+        }
+    })
+})
